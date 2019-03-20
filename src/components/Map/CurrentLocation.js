a71bf1158cfa336164ba7d4fbc9c6f2967f344dd
Marcus Anderberg <marcusanderberg@gmail.com>
2019-03-20 13:56:44 -0500

    working on maps

---
+++ src/components/Map/CurrentLocation.js
@@ -0,0 +1,98 @@
+// import React from 'react';
+// // import GoogleMapReact from 'google-maps-react';
+// // import Geocode from "react-geocode";
+// import { GoogleApiWrapper, Map } from "google-maps-react";
+
+
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+
+
+
+
+// class SimpleMap extends Component {
+//   static defaultProps = {
+//     center: {
+//       lat: 44.97,
+//       lng: -93.26,
+//     },
+//     zoom: 12
+//   };
+
+
+
+
+
+//   render() {
+//     return (
+//       // Important! Always set the container height explicitly
+//       <div className="map">
+//         <GoogleMapReact
+//           bootstrapURLKeys={{ key: 'AIzaSyDf4NU_rGOmsUaQj0HVAQsV2Uxb0xieWY0'}}
+//           defaultCenter={this.props.center}
+//           defaultZoom={this.props.zoom}
+//         >
+//           <AnyReactComponent
+//             lat={44.9778}
+//             lng={-93.2650}
+
+//           />
+//         </GoogleMapReact>
+//       </div>
+//     );
+//   }
+// }
+
+// export default SimpleMap;
+
+
+import React, { Component } from 'react';
+import { GoogleApiWrapper, Map } from "google-maps-react";
+
+class MapContainer extends Component {
+
+
+    state = {
+        userLocation: {
+            lat: 32,
+            lng: 32
+        },
+        loading: true
+    };
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const { latitude, longitude } = position.coords;
+
+                this.setState({
+                    userLocation: { lat: latitude, lng: longitude },
+                    loading: false,
+                });
+                new window.google.maps.Marker({
+                    position: { lat: latitude, lng: longitude },
+                    map: Map,
+                });
+            },
+            () => {
+                this.setState({ loading: false });
+            }
+        );
+    }
+
+    render() {
+        const { loading, userLocation } = this.state;
+        const { google } = this.props;
+
+        if (loading) {
+            return null;
+        }
+
+        return <Map google={google} initialCenter={userLocation} zoom={15} />;
+
+    }
+}
+
+export default GoogleApiWrapper({
+    apiKey: "AIzaSyDf4NU_rGOmsUaQj0HVAQsV2Uxb0xieWY0"
+})(MapContainer);
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,81 +1,64 @@
-import React from 'react';
-// import GoogleMapReact from 'google-maps-react';
-// import Geocode from "react-geocode";
-import { GoogleApiWrapper, Map } from "google-maps-react";
-
-
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import React, {Component} from 'react';
 
 
 
 
 
-// class SimpleMap extends Component {
-//   static defaultProps = {
-//     center: {
-//       lat: 44.97,
-//       lng: -93.26,
-//     },
-//     zoom: 12
-//   };
 
 
+class Map extends Component {
 
+  componentDidMount() {
+    this.renderMap()
+  }
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDf4NU_rGOmsUaQj0HVAQsV2Uxb0xieWY0&callback=initMap" )
+    window.initMap = this.initMap
+  }
 
   
-//   render() {
-//     return (
-//       // Important! Always set the container height explicitly
-//       <div className="map">
-//         <GoogleMapReact
-//           bootstrapURLKeys={{ key: 'AIzaSyDf4NU_rGOmsUaQj0HVAQsV2Uxb0xieWY0'}}
-//           defaultCenter={this.props.center}
-//           defaultZoom={this.props.zoom}
-//         >
-//           <AnyReactComponent
-//             lat={44.9778}
-//             lng={-93.2650}
-           
-//           />
-//         </GoogleMapReact>
-//       </div>
-//     );
-//   }
-// }
 
-// export default SimpleMap;
+  initMap = () => {
+    var myLatLng = { lat: 44.97, lng: -93.26 };
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: myLatLng
+    });
+  
 
-export class MapContainer extends React.Component {
-  state = { userLocation: { lat: 32, lng: 32 }, loading: true };
-
-  componentDidMount(props) {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const { latitude, longitude } = position.coords;
-
-        this.setState({
-          userLocation: { lat: latitude, lng: longitude },
-          loading: false
-        });
-      },
-      () => {
-        this.setState({ loading: false });
-      }
-    );
+    let marker = new window.google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Hello World!'
+    });
   }
 
-  render() {
-    const { loading, userLocation } = this.state;
-    const { google } = this.props;
 
-    if (loading) {
-      return null;
-    }
+ 
 
-    return <Map google={google} initialCenter={userLocation} zoom={15} />;
-  }
+ render() {
+  return (
+    <main>
+      <div id="map"></div>
+    </main>
+  )
+ }
 }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyDf4NU_rGOmsUaQj0HVAQsV2Uxb0xieWY0"
-})(MapContainer);
+
+
+export default Map;
+
+
+
+
+function loadScript(url) {
+  let index = window.document.getElementsByTagName("script")[0]
+  let script = window.document.createElement("script")
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+
+}
